11f23bdd806410d71dd81e16ad2147238b4fcc4a
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-05-02 18:22:26 -0500

    Agrego el codigo para generar una nueva card

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 require('dotenv').config();
 
 if (!process.env.TOKEN && !process.env.KEY) {
-  trow new Error('No hay configuración con API Key y Token');
+  throw new Error('No hay configuración con API Key y Token');
 }
 
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
 
+let cardTitle = `Card Nueva ${new Date()}`;
+
+trello.addCard(cardTitle, "My Card Description", "62704a8959513806c62c0fbe", 
+  function(error, trelloCard) {
+    if (error) {
+      console.log('Could not add card:', error);
+    }
+    else {
+      console.log('Added Card', trelloCard);
+    }
+  });
